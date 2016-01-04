2b9849357c73b4274cdda9d0a7a4f3d4bd2e15bb
alex.lee <lyd.alexlee@gmail.com>
2016-01-04 15:50:46 +0800

    fixed RedisConsumer race condition.

--- totoro/redis.py
+++ totoro/redis.py
@@ -8,7 +8,7 @@ from functools import partial
 
 from tornado import gen
 from tornado import ioloop
-from tornadoredis import Client, ConnectionPool
+from tornadoredis import Client
 from tornadoredis.exceptions import ResponseError
 from tornadoredis.pubsub import SocketIOSubscriber
 from redis import StrictRedis
@@ -37,31 +37,49 @@ class RedisClient(Client):
 
 
 class RedisConsumeDelegate(object):
+    SUBSCRIBER_INIT = 0
+    SUBSCRIBER_SUBSCRIBING = 1
+    SUBSCRIBER_SUBSCRIBED = 2
+
     def __init__(self, key, callback, subscriber, io_loop=None, oneshot=True):
         self._key = key
         self._callback = callback
         self._subscriber = subscriber
         self._io_loop = io_loop or ioloop.IOLoop.instance()
         self._oneshot = oneshot
-        self._subscribed = False
+        self._subscriber_state = self.SUBSCRIBER_INIT
         self._consume_callbacks = list()
 
     @property
-    def subscribed(self):
-        return self._subscribed
+    def is_subscribed(self):
+        return self._subscriber_state == self.SUBSCRIBER_SUBSCRIBED
+
+    @property
+    def is_subscribing(self):
+        return self._subscriber_state == self.SUBSCRIBER_SUBSCRIBING
+
+    def _set_subscriber_state(self, subscriber_state):
+        self._subscriber_state = subscriber_state
 
     def _update_subscribe_state(self, future):
-        self._subscribed = future.result()
-        if self._subscribed:
-            LOGGER.info('The redis subscription success(key={0}).'.format(self._key))
+        if future.result():
+            if self.is_subscribing:
+                LOGGER.debug('The redis subscription success: {0}.'.format(self._key))
+                self._set_subscriber_state(self.SUBSCRIBER_SUBSCRIBED)
+            else:
+                LOGGER.info('The redis subscription have been cancelled before '
+                            'seeing success: {0}.'.format(self._key))
+        else:
+            LOGGER.warning('The redis subscription fails: {0}.'.format(self._key))
 
     def add_consume_callback(self, callback, *args, **kwargs):
         if callback:
             self._consume_callbacks.append(partial(callback, *args, **kwargs))
 
     def consume(self):
-        if not self._subscribed:
-            LOGGER.debug('Start a redis subscription(key={0}).'.format(self._key))
+        if self._subscriber_state == self.SUBSCRIBER_INIT:
+            LOGGER.debug('Start a redis subscription: {0}.'.format(self._key))
+            self._set_subscriber_state(self.SUBSCRIBER_SUBSCRIBING)
             self._io_loop.add_future(
                 gen.Task(self._subscriber.subscribe, self._key, self),
                 self._update_subscribe_state)
@@ -70,7 +88,7 @@ class RedisConsumeDelegate(object):
         if delete_consume_callbacks:
             del self._consume_callbacks[:]
         self._subscriber.unsubscribe(self._key, self)
-        self._subscribed = False
+        self._set_subscriber_state(self.SUBSCRIBER_INIT)
 
     def on_message(self, msg):
         try:
@@ -78,6 +96,7 @@ class RedisConsumeDelegate(object):
                 [cb() for cb in self._consume_callbacks]
             if self._callback:
                 self._callback(msg)
+                LOGGER.debug('Consumed a message: {0}'.format(self._key))
         finally:
             if self._oneshot:
                 self.cancel()
@@ -86,11 +105,11 @@ class RedisConsumeDelegate(object):
 class RedisConsumer(TaskConsumerBase):
     def __init__(self, **kwargs):
         super(RedisConsumer, self).__init__(**kwargs)
-        self.redis_client = StrictRedis(
-            host=self.backend.connparams['host'],
-            port=self.backend.connparams['port'],
-            password=self.backend.connparams['password'],
-            db=self.backend.connparams['db'])
+        self.host = self.backend.connparams['host']
+        self.port = self.backend.connparams['port']
+        self.password = self.backend.connparams['password']
+        self.selected_db = self.backend.connparams['db']
+        self.redis_client = StrictRedis(self.host, self.port, self.selected_db, self.password)
         self.subscriber = self.create_redis_subscriber()
 
     def _wait_for(self, task_id, callback, wait_timeout):
@@ -108,22 +127,21 @@ class RedisConsumer(TaskConsumerBase):
         def _check_subscribed():
             result = self.redis_client.get(key)
             if result:
-                LOGGER.info('Get the result before redis subscribed: key={0}.'.format(key))
+                LOGGER.info('Get the result before redis subscribed: {0}.'.format(key))
                 consume_delegate.on_message(result)
-            elif not consume_delegate.subscribed:
-                LOGGER.debug('Continue to check the subscription status: key={0}.'.format(key))
+            elif consume_delegate.is_subscribing:
+                LOGGER.debug('Continue to check the subscriber state: {0}.'.format(key))
+                # self.io_loop.add_callback(_check_subscribed)
                 self.io_loop.add_timeout(timedelta(seconds=1), _check_subscribed)
+            else:
+                LOGGER.info('Stop checking the subscriber state: {0}.'.format(key))
         self.io_loop.add_callback(_check_subscribed)
 
     def create_redis_subscriber(self):
-        connection_pool = ConnectionPool(
-            max_connections=1024,
-            host=self.backend.connparams['host'],
-            port=self.backend.connparams['port'],
-            io_loop=self.io_loop)
         redis_client = RedisClient(
-            password=self.backend.connparams['password'],
-            selected_db=self.backend.connparams['db'],
-            connection_pool=connection_pool,
+            host=self.host,
+            port=self.port,
+            password=self.password,
+            selected_db=self.selected_db,
             io_loop=self.io_loop)
         return SocketIOSubscriber(redis_client)
